ab10fdcc40afe44a6182d2d98a1f4d932422b5b9
pvalenta <patrick.valenta@hs-augsburg.de>
2017-05-02 08:10:02 +0200

    Layout fix when opening app in landscape

--- sammelAmpel/AppDelegate.swift
+++ sammelAmpel/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        application.statusBarOrientation = .portrait
+        
         FIRApp.configure()
         
         window = UIWindow(frame: UIScreen.main.bounds)
